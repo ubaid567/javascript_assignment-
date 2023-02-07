86003b3038524f06d8948031e284e155ed190106
ubaid567 <124505833+ubaid567@users.noreply.github.com>
2023-02-07 02:48:55 -0800

    assignment 3
    chapter 5 & 6-9

---
+++ app.js
@@ -0,0 +1,236 @@
+//chapter#5 MATH EXPRESSIONS
+
+
+//question#1
+//var num=5
+//var num1= 3
+//var numf=num+num1//
+//document.write(
+//  "sum of "+num+" and "+num1+" is "+numf
+//)
+
+
+//question#3
+//var num=5
+//var num1= 3
+//var numf=num/num1
+//document.write(
+//  "division of "+num+" and "+num1+" is "+numf
+//)
+
+//var num=5
+//var num1= 3
+//var numf=num*num1
+//document.write(
+//  "multiply of "+num+" and "+num1+" is "+numf
+//)
+
+//var num=5
+//var num1= 3
+//var numf=num-num1
+//document.write(
+//  "subtract of "+num+" and "+num1+" is "+numf
+//)
+
+//question#3
+//var start=5
+//document.write(start );
+//start=++start
+//document.write(start);
+//start=start+7
+//document.write(start);
+//start=--start
+//document.write(start);
+//start=start/4
+//document.write(start);
+
+
+//question#4
+//var ticketPrice=1100
+//alert("1 ticket price is 1100 pkr")
+//var count=prompt("enter a quantity of ticket")
+//document.write(
+//  "total cost to buy  "+count+ "ticket to a match is "+ticketPrice*count+ " PKR"
+//)
+
+
+//question#5
+
+//var first="5"
+
+//document.write( "<h1>"+first+ "x 1= 5 </h1>")
+//document.write( "<h1>"+first+ "x 2= 10 </h1>")
+//document.write( "<h1>"+first+ "x 3= 15 </h1>")
+//document.write( "<h1>"+first+ "x 4= 20 </h1>")
+//document.write( "<h1>"+first+ "x 5= 25 </h1>")
+//document.write( "<h1>"+first+ "x 6= 30 </h1>")
+//document.write( "<h1>"+first+ "x 7= 35 </h1>")
+//document.write( "<h1>"+first+ "x 8= 40 </h1>")
+//document.write( "<h1>"+first+ "x 9= 45 </h1>")
+//document.write( "<h1>"+first+ "x 10= 50 </h1>")
+
+
+//qustion#6
+
+//var c=25
+//var f=70
+//var cf=(f-32)*5/9
+//var fc=(c*5/9)+32
+
+//document.write("<p>"+c+"°C is "+cf+"°F</p>")
+//document.write( f+"°F is "+cf+"°C")
+
+
+//queston#7
+
+//var bad=2450
+//var bowl=250
+//var quantityOfbad=2
+//var quantityOfbowl=7
+//var shipping=350
+//document.write(
+//  "<h1> Shopping cart </h1>"+
+//  "<p>price of 1 bat is "+bad+" </p>"+
+//  "<p>Quantity of bats is "+quantityOfbad+"</p>"+
+//  "<p>price of 1 bowl is "+bowl+"</p>"+
+//  "<p>Quantity of bowls is "+quantityOfbowl+"</p>"+
+//  "<p>Shipping charges is  "+shipping+"</p>"+
+
+// "<h3> total cost of your order is "+bad*quantityOfbad + bowl*quantityOfbowl+shipping+"</h3>"
+//)
+
+
+//question#8
+
+//var totalMarks=870
+//var obtainedMarks=730
+//var totalPercentage=obtainedMarks*100/totalMarks
+//document.write("<h1>Mark Sheet</h1>")
+//document.write("<h3>Total Marks : "+totalMarks+"</h3>")
+//document.write("<h3>Obtained Marks : "+obtainedMarks+"</h3>")
+//document.write("<h3>percentage : "+totalPercentage+"</h3>")
+
+
+//question#9
+
+//var usdDollar=prompt("Enter how much usd dollar do you want to cash ")
+//var saudiRiyal=prompt("Enter how much saudi riyal do you want to cash")
+//var totalPkr=usdDollar*277+saudiRiyal*66
+//document.write("<h1>Currency in PKR </h1>")
+//document.write("Total Currency in PKR :"+totalPkr)
+
+
+//question#10
+
+//alert(((5+5)*10)/2)
+
+//question#11
+//var currentyear=2023
+//var birthYear=prompt("Enter your birth year")
+//var yourAge=currentyear-birthYear
+//document.write("<h1>Age Calculater</h1>")
+//document.write("current year :"+currentyear+"<br/>")
+//document.write("birth year :"+birthYear+"<br/>")
+//document.write("your age is :"+yourAge)
+
+
+
+//question#13
+
+//var fs= prompt("Enter you favourake snack name" )
+//document.write("<h1>Life Time Supply Calculater</h1>")
+//document.write("Your favourite snack : "+fs+"<br/>")
+//var cAge=prompt("Enter your current age")
+//document.write("Current Age :"+cAge+"</br>")
+//var eage= cAge*7
+//document.write("Estimated Maximum Age :"+ eage+"<br/>")
+//var pds=prompt("How much snacks do you eat in a day")
+//document.write( "Amount of snacks per day :"+ pds+"<br/>")
+//var final=( eage-cAge )*pds
+//document.write("you will need "+ final + fs+ " to last you until the ripe of old age of "+eage )
+
+
+
+
+
+//chapter#6-9 MATH EXPRESSIONS
+
+//question#1
+
+//var a=10
+//document.write("the value of a is :"+a+"<br/>")
+
+//document.write("the value of ++a is :"+ ++a+"<br/>")
+//document.write("now the value of a is :"+a+"<br/>")
+
+//document.write("the value of "+ "a++" + "is :"+a++ +"<br/>")
+//document.write("now the value of a is :"+ a +"<br/>")
+
+
+//document.write(" the value of --a is :"+ --a +"<br/>")
+//document.write("now the value of a is :"+ a +"<br/>")
+
+
+//document.write(" the value of a is :"+ a-- +"<br/>")
+//document.write("now the value of a is :"+ a +"<br/>")
+
+
+//question#2
+
+//var a=2
+//var b=1
+//var result=--a - --b + ++b + b--
+//result is 3
+//var explain1=1
+//var explain2=1
+//var explain3=3
+//var explain4=3
+
+
+//question#3
+//var name= prompt("enter your name")
+//alert(" assalam-walikum " +name)
+
+
+//question#5
+//var table=prompt("Enter a any number")
+//if( table ==""){
+//document.write(5 +" x "+"1 "+"= "+5*1+"<br/>")
+//document.write(5 +" x "+"2 "+"= "+5*2+"<br/>")
+//document.write(5 +" x "+"3 "+"= "+5*3+"<br/>")
+//document.write(5 +" x "+"4 "+"= "+5*4+"<br/>")
+//document.write(5 +" x "+"5 "+"= "+5*5+"<br/>")
+//document.write(5 +" x "+"6 "+"= "+5*6+"<br/>")
+//document.write(5 +" x "+"7 "+"= "+5*7+"<br/>")
+//document.write(5 +" x "+"8 "+"= "+5*8+"<br/>")
+//document.write(5 +" x "+"9 "+"= "+5*9+"<br/>")
+// document.write(5 +" x "+"10 "+"= "+5*10+"<br/>")
+//}
+//else{
+
+//document.write(table +" x "+"1 "+"= "+table*1+"<br/>")
+//document.write(table +" x "+"2 "+"= "+table*2+"<br/>")
+//document.write(table +" x "+"3 "+"= "+table*3+"<br/>")
+//document.write(table +" x "+"4 "+"= "+table*4+"<br/>")
+//document.write(table +" x "+"5 "+"= "+table*5+"<br/>")
+//document.write(table +" x "+"6 "+"= "+table*6+"<br/>")
+//document.write(table +" x "+"7 "+"= "+table*7+"<br/>")
+//document.write(table +" x "+"8 "+"= "+table*8+"<br/>")
+//document.write(table +" x "+"9 "+"= "+table*9+"<br/>")
+//document.write(table +" x "+"10 "+"= "+table*10+"<br/>")
+//}
+
+
+//quetion#6
+
+
+var subject1 = prompt("enter first subject")
+var subject2 = prompt("enter second subject")
+var subject3 = prompt("enter third subject")
+var subject1Number = prompt("enter first subject obtained number")
+var subject2Number = prompt("enter second subject obtained number")
+var subject3Number = prompt("enter third subject obtained number")
+document.write(
+  "<table border=2><tr><th>subject</th><th>total marks</th><th>obtained marks</th><th>percentage</th></tr>" + "<tr><th> " + subject1 + "</th> <th> " + 100 + "</th><th> " + subject1Number + "</th><th> " + subject1Number * 100 / 100 + "%" + "</th></tr>" +"<tr><th> " + subject2 + "</th> <th> " + 100 + "</th><th> " + subject2Number + "</th><th> " + subject2Number * 100 / 100 + "%" + "</th></tr>"+"<tr> <th> " + subject3 + "</th> <th> " + 100 + "</th><th> " + subject3Number + "</th><th> " + subject3Number * 100 / 100 + "%" + "</th></tr>"  + "</table>"
+)
+
